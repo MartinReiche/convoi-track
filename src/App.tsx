b87674387626abe11a01da5324be9c0b90e84d26
Martin Reiche <martin@reiche.dev>
2022-03-07 07:34:16 +0100

    Added favicon Assets and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Layout from './layout';
 import Map from './components/map';
+import Login from './pages/login';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+
+} from "react-router-dom";
 
 function App() {
     return (
         <Layout>
-            <Map />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/map" element={<Map />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/convoys" element={<Login />} />
+                    <Route path="/convoys/:convoyId" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+
         </Layout>
     );
 }
